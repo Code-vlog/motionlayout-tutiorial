c87532258e217e88ca826b3261591a65c0140cc1
work-kiku1007 <work.sourov@gmail.com>
2020-07-10 09:03:18 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/goodwilldev/motionlayout_tutiorial/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.goodwilldev.motionlayout_tutiorial
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.goodwilldev.motionlayout_tutiorial", appContext.packageName)
+    }
+}
