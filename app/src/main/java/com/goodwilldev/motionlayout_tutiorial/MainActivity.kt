1ce518b4c68f88febcf9d8ef52b33e5fa4e96cc4
work-kiku1007 <work.sourov@gmail.com>
2020-07-11 23:08:06 +0530

    starter

--- app/src/main/java/com/goodwilldev/motionlayout_tutiorial/MainActivity.kt
+++ app/src/main/java/com/goodwilldev/motionlayout_tutiorial/MainActivity.kt
@@ -2,17 +2,10 @@ package com.goodwilldev.motionlayout_tutiorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.goodwilldev.motionlayout_tutiorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.textField.setStartIconOnClickListener {
-            binding.imageView.performClick()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
